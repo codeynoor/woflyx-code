f2869695654e07990e7dddb5036982c51028481d
itzAceOfficial <68320326+itzAceOfficial@users.noreply.github.com>
2020-12-20 15:27:29 +0600

    Create anime.js

---
+++ commands/anime.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const Kitsu = require('kitsu.js');
+const kitsu = new Kitsu();
+var aq = require('animequote');
+const fetch = require("node-fetch");
+const { execute } = require('./lick');
+
+module.exports =  {
+  name: "anime",
+  description: "anime",
+  async execute(message, args, client) {
+
+   if (!args[0]) {
+    message.channel.send("what anime do you want to search??");
+    return;
+    }
+
+        var search = message.content.split(/\s+/g).slice(1).join(" ");
+        kitsu.searchAnime(search).then(async result => {
+            if (result.length === 0) {
+                return message.channel.send(`:no_entry_sign: | oop no results found for **${search}** .-.`);
+            }
+          
+          var anime = result[0]
+
+            let embed = new Discord.MessageEmbed()
+                .setColor('#FF2050')
+                .setAuthor(`${anime.titles.english ? anime.titles.english : search} | ${anime.showType}`, anime.posterImage.original)
+                .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
+                .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating || "Couldn't find rating"}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
+                .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
+                .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
+            
+                .setThumbnail(anime.posterImage.original, 100, 200);
+          
+
+            return message.channel.send({ embed })
+        }).catch(err => {
+            console.log(err) 
+            return message.channel.send(`:no_entry_sign: | Something went wrong!!`);
+        });
+    } 
+}
